af807b0f1b52c6ac87f0a5a2963b427ac8a29e73
Eugene Rodin <eugene.rodin1@gmail.com>
2016-10-20 22:42:14 +0300

    created draggable and droppable directives, some debug stuff

--- public/javascripts/app/directives.js
+++ public/javascripts/app/directives.js
@@ -1,13 +1,15 @@
-(function() {
+(function () {
 
 	angular.module('dndTreeApp.directives')
-		.directive('ngDomManipulator', ngDomManipulator);
+		.directive('ngDomManipulator', ngDomManipulator)
+		.directive('ngDraggable', ngDraggable)
+		.directive('ngDroppable', ngDroppable);
 
 	ngDomManipulator.$inject = [];
 
 	function ngDomManipulator() {
 
-		var isEmpty = function(object) {
+		var isEmpty = function (object) {
 
 			for (var key in object) {
 				return false;
@@ -21,13 +23,16 @@
 				if (tree[branch] instanceof Array) {
 					li.innerHTML = branch;
 					li.className = "tree__array";
+					// li.draggable= true;
 				} else if (tree[branch] instanceof Object) {
 					li.innerHTML = branch;
 					li.className = "tree__object";
+					// li.draggable= true;
 				} else {
 					var p = document.createElement('p');
 					p.className = 'tree__p';
 					p.innerHTML = tree[branch];
+					// p.draggable= true;
 					li.innerHTML = branch;
 					li.appendChild(p);
 				}
@@ -48,8 +53,8 @@
 			scope: {
 				tree: '='
 			},
-			link: function(scope, elt, attrs) {
-				scope.$watch('tree', function(data) {
+			link: function (scope, elt, attrs) {
+				scope.$watch('tree', function (data) {
 					if (isEmpty(data)) return;
 					elt.append(angular.element('<ul></ul>'));
 					createTree(data, document.getElementsByTagName('ul')[0]);
@@ -58,33 +63,118 @@
 		}
 	}
 
+	ngDraggable.$inject = [];
+
+	function ngDraggable() {
+		return {
+			restrict: 'A',
+			link: function (scope, elt, attrs) {
+				elt.on('mouseover', function (e) {
+					var touchedElt = e.target || event.target;
+					touchedElt.draggable = true;
+					// console.log(`touchedElt: ${touchedElt}, draggable: ${touchedElt.draggable}`);
+
+					touchedElt.addEventListener(
+						'dragstart',
+						function (e) {
+							var _this = e.target.parentNode;
+							_this.id = 'dragged';
+							e.dataTransfer.effectAllowed = 'move';
+							e.dataTransfer.setData('Text', _this.id);
+							_this.classList.add('drag');
+							//debug
+							e.target.parentNode.style.opacity = '.5';
+							// console.log(_this.classList);
+							return false;
+						},
+						false
+					);
+
+					touchedElt.addEventListener(
+						'dragend',
+						function (e) {
+							var _this = e.target.parentNode;
+							_this.classList.remove('drag');
+							// console.log(_this.classList);
+							return false;
+						},
+						false
+					);
+
+				});
+			}
+		}
+	}
+
+	ngDroppable.$inject = [];
+	function ngDroppable() {
+		return {
+			restrict: 'A',
+			link: function (scope, elt, attrs) {
+				elt.on('mouseover', function (e) {
+					var hoveredElt = e.target;
+					hoveredElt.classList.add('navigator');
+					// console.log(hoveredElt);
+					hoveredElt.addEventListener(
+						'dragover',
+						function (e) {
+							var _this = e.target.parentNode;
+							e.dataTransfer.dropEffect = 'move';
+							// allows us to drop
+							if (e.preventDefault) e.preventDefault();
+							_this.classList.add('over');
+							return false;
+						},
+						false
+					);
+
+					hoveredElt.addEventListener(
+						'dragenter',
+						function (e) {
+							var _this = e.target.parentNode;
+							_this.classList.add('over');
+							return false;
+						},
+						false
+					);
+
+					hoveredElt.addEventListener(
+						'dragleave',
+						function (e) {
+							var _this = e.target.parentNode;
+							_this.classList.remove('over');
+							return false;
+						},
+						false
+					);
+
+					hoveredElt.addEventListener(
+						'drop',
+						function (e) {
+							// var _this = e.target.parentNode;
+							if (e.stopPropagation) e.stopPropagation();
+
+							this.classList.remove('over');
+
+							var item = document.getElementById(e.dataTransfer.getData('Text'));
+							console.log(`this: ${this} item: ${item}`);
+							this.appendChild(item);
+
+							return false;
+						},
+						false
+					);
+
+					hoveredElt.addEventListener('mouseout', function (e) {
+						e.target.classList.remove('navigator');
+					});
+				});
+
+
+			}
+		}
+	}
+
+
 })();
-// var createDOMTree = function(data) {
-// 			var mainUl = document.createElement('ul');
 
-// 			var createTree = function(data, listElement) {
-// 				var newLI = document.createElement('li');
-
-// 				for (var key in data) {
-// 					if (data[key] instanceof Array) {
-// 						newLI.innerHTML = key;
-// 						newLI.className = "array";
-// 					} else if (data[key] instanceof Object) {
-// 						newLI.innerHTML = key;
-// 						newLI.className = "object";
-// 					} else
-// 						newLI.innerHTML = key + ': ' + data[key];
-
-// 					listElement.appendChild(newLI);
-
-// 					if (data[key] instanceof Array || data[key] instanceof Object) {
-// 						var newUL = document.createElement('ul');
-// 						listElement.appendChild(newUL);
-// 						createTree(data[key], newUL);
-// 					}
-
-// 				}
-
-// 			};
-// 			return mainUl;
-// 		}
